aba4f2280e4fd22464a5f7f036431ec0e3fa200c
KalionCode <xxfaya.dragonxx@gmail.com>
2022-03-30 12:27:00 +1300

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,126 @@
+function EnterHandler() {
+  console.log('enter pressed');
+  // Get message text
+  let msg = chatbox.root.innerHTML;
+
+  msg = msg.trim();
+
+  if (msg == "<p><br></p>") {
+    return false;
+  }
+
+  // Emit message to server
+  socket.emit('chatMessage', msg);
+
+  // Clear input
+  chatbox.root.innerHTML = '';
+  chatbox.root.focus();
+}
+
+var chatbox = new Quill('#chatbox', {
+  theme: "bubble",
+  bounds: document.querySelector("#chatbox"),
+  placeholder: "Enter your message here",
+});
+
+chatbox.keyboard.addBinding(
+  { key: 13, shiftKey:null },
+  EnterHandler
+)
+
+const summitBtn = document.getElementById('summit-btn');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+const socket = io();
+
+// Join chatroom
+socket.emit('joinRoom', { username, room });
+
+socket.on('duplicateName', () => {
+  alert("Another user with the same name already joined, please choose another name")
+  window.location = '/'
+});
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+// Message from server
+socket.on('message', (message) => {
+  console.log(message);
+  outputMessage(message);
+
+  // Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+// Message submit
+summitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  // Get message text
+  let msg = chatbox.root.innerHTML;
+
+  msg = msg.trim();
+
+  if (msg == "<p><br></p>") {
+    return false;
+  }
+
+  // Emit message to server
+  socket.emit('chatMessage', msg);
+
+  // Clear input
+  chatbox.root.innerHTML = '';
+  chatbox.root.focus();
+});
+
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  const p = document.createElement('p');
+  p.classList.add('meta');
+  p.innerText = message.username;
+  p.innerHTML += `<span> ${message.time}</span>`;
+  div.appendChild(p);
+  const para = document.createElement('p');
+  para.classList.add('text');
+  para.innerHTML = message.text;
+  div.appendChild(para);
+  document.querySelector('.chat-messages').appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = '';
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
+}
+
+//Prompt the user before leave chat room
+document.getElementById('leave-btn').addEventListener('click', () => {
+  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+  if (leaveRoom) {
+    window.location = '../index.html';
+  } else {
+  }
+});
+
---
+++ utils/users.js
@@ -0,0 +1,46 @@
+const users = [];
+
+// Join user to chat
+function userJoin(id, username, room) {
+  const user = { id, username, room };
+
+  users.push(user);
+
+  return user;
+}
+
+// Get current user
+function getCurrentUser(id) {
+  return users.find(user => user.id === id);
+}
+
+// User leaves chat
+function userLeave(id) {
+  const index = users.findIndex(user => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+// Get room users
+function getRoomUsers(room) {
+  return users.filter(user => user.room === room);
+}
+function getRoomUsernames(room) {
+  let allUsers = users.filter(user => user.room === room);
+  let allUsernames = [];
+  for (user of allUsers){
+    allUsernames.push(user.username)
+  }
+  return allUsernames;
+}
+
+module.exports = {
+  users,
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+  getRoomUsernames,
+};
